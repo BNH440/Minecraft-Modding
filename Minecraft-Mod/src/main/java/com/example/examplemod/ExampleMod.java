6075e6ae5db90cb6949319d602e1008c5e5b01c9
Blake Haug <bnh440@icloud.com>
2020-04-12 09:08:46 -0700

    Mod won't load texture or lang

--- Minecraft-Mod/src/main/java/com/example/examplemod/ExampleMod.java
+++ Minecraft-Mod/src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,31 +1,33 @@
 package com.example.examplemod;
 
 import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
+import net.minecraftforge.common.util.EnumHelper;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
+@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
 public class ExampleMod
 {
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
+    public static final String MODID = "sword";
+    public static final String NAME = "Cool Sword";
     public static final String VERSION = "1.0";
 
-    private static Logger logger;
+    public static Item.ToolMaterial myToolMaterial;
+    public static Item mySword;
 
     @EventHandler
-    public void preInit(FMLPreInitializationEvent event)
-    {
-        logger = event.getModLog();
+    public void preInit(FMLPreInitializationEvent event){
+        myToolMaterial = EnumHelper.addToolMaterial("Flex Tape Sword", 4, 100000, 20.0F, 100.0F, 30);
+        mySword = new CustomSword();
     }
 
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
         // some example code
-        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+        System.out.println("DIRT BLOCK >> " + Blocks.DIRT.getUnlocalizedName());
     }
 }
