c9fd9105e678ecae9f8b67b8bdea300c3b9df951
Blake Haug <bnh440@icloud.com>
2020-04-13 17:43:56 -0700

    Not working, Game crashes. Cause: Ticking Entity

--- Minecraft-Mod-Eclipse/src/main/java/com/example/examplemod/CommonProxy.java
+++ Minecraft-Mod-Eclipse/src/main/java/com/example/examplemod/CommonProxy.java
@@ -1,15 +1,27 @@
 package com.example.examplemod;
 
 import net.minecraft.block.Block;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.EnumHand;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.event.entity.living.LivingAttackEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraft.entity.passive.EntityBat;
 
 import java.rmi.registry.Registry;
 
@@ -44,5 +56,18 @@ public class CommonProxy {
     	registerRenderItem(ExampleMod.flexPaste);
     	registerRenderItem(Item.getItemFromBlock(ExampleMod.flexBlock));
     	registerRenderItem(ExampleMod.slimeStick);
+    }
+    
+    @SubscribeEvent
+    public static void onHurt(LivingAttackEvent event) {
+    	String damageType = event.getSource().getDamageType();
+    	EntityLivingBase victimEntity = event.getEntityLiving();
+    	Entity attackingEntity = event.getSource().getTrueSource();
+    	ItemStack heldItem = ((EntityLivingBase)attackingEntity).getHeldItem(EnumHand.MAIN_HAND);
+    	if(attackingEntity instanceof EntityPlayer) {
+    		if(heldItem == new ItemStack(ExampleMod.mySword)){
+    			victimEntity.addPotionEffect(new PotionEffect(Potion.getPotionById(2), 100, 255));
+    		}
+    	}
     }
 }
--- Minecraft-Mod-Eclipse/src/main/java/com/example/examplemod/ExampleMod.java
+++ Minecraft-Mod-Eclipse/src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,15 +1,21 @@
 package com.example.examplemod;
 
+import com.google.common.eventbus.Subscribe;
+
 import net.minecraft.block.Block;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
+import net.minecraftforge.client.MinecraftForgeClient;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.util.EnumHelper;
+import net.minecraftforge.event.entity.living.LivingAttackEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
 public class ExampleMod
@@ -26,7 +32,7 @@ public class ExampleMod
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event){
-        swordMaterial = EnumHelper.addToolMaterial("my_sword", 4, 100000, 20.0F, 100.0F, 30);
+        swordMaterial = EnumHelper.addToolMaterial("my_sword", 4, 1000, 20.0F, 10.0F, 30);
         mySword = new CustomSword();
         flexPaste = new FlexPaste();
         flexBlock = new FlexBlock();
@@ -40,13 +46,5 @@ public class ExampleMod
         System.out.println("DIRT BLOCK >> " + Blocks.DIRT.getUnlocalizedName());
     }
     
-    @EventHandler
-    public void whenHurt(LivingHurtEvent event) {
-    	if(event.getEntity() instanceof EntityPlayer) {
-    		System.out.println("Entity is Living: " + event.getEntity().getName());
-    	}
-    	else {
-    		System.out.println("Entity is not Living");
-    	}
-    }
+    
 }
